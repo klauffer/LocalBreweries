a4c6ff62e8e3a6cfe9187d8a25d53586eb934a8f
Kyle Lauffer <kcl5072@gmail.com>
2021-08-10 16:03:11 -0400

    Adding bootstrap and scss

--- local-breweries/src/App.tsx
+++ local-breweries/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { BreweriesSearchResults } from "./BreweriesSearchResults";
 
 const App: React.FC = () => {
--- local-breweries/src/BrewerySearchResult.tsx
+++ local-breweries/src/BrewerySearchResult.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import IBrewerySummary from "./types/brewerySummary.type";
+import { Card, Button } from "react-bootstrap";
 
 interface Props {
     BrewerySummary: IBrewerySummary
 }
 
 
-export const BrewerySearchResult: React.FC<Props> = ({BrewerySummary}) => {
+export const BrewerySearchResult: React.FC<Props> = ({ BrewerySummary }) => {
     return (
-        <div>
-            <h2>{BrewerySummary.Name}</h2>
-            <h3>{BrewerySummary.BreweryType}</h3>
-            <p>{BrewerySummary.URL}</p>
-        </div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>{BrewerySummary.Name}</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                </Card.Text>
+                <Button variant="primary">See More!</Button>
+            </Card.Body>
+        </Card>
     );
 };
 
