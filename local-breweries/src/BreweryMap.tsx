9f2c219f044d2d9f0cca7d048f62d8c39513bee9
Kyle Lauffer <kcl5072@gmail.com>
2021-08-12 18:35:41 -0400

    more map updated but google map is being difficult like the final level of contra

--- local-breweries/src/BreweryMap.tsx
+++ local-breweries/src/BreweryMap.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import ICoordinate from "./types/coordinate.type";
 import GoogleMapReact from 'google-map-react';
-import GoogleMap from 'google-map-react';
 import BreweryMapMarker from "./BreweryMapMarker";
 
 interface Props {
@@ -9,13 +8,17 @@ interface Props {
 };
 
 const BreweryMap: React.FC<Props> = ({ Coordinate }) => {
+    const mapRef = useRef(null);
+    
+    const [position, setPosition] = useState({
+        lat: +Coordinate.Latitude,
+        lng: +Coordinate.Longitude
+    });
 
-    function GetCenter(): GoogleMapReact.Coords {
-        return {
-            lat: +Coordinate.Latitude,
-            lng: +Coordinate.Longitude
-        }
-    }
+    function handleLoad(map : any) {
+        mapRef.current = map;
+      }
+
 
     function GetAPIKey() : string {
         if(process.env.REACT_APP_GOOGLE_MAP_API_KEY !== undefined){
@@ -27,11 +30,15 @@ const BreweryMap: React.FC<Props> = ({ Coordinate }) => {
     return (
         <div>
             {/* {Coordinate.Latitude} , {Coordinate.Longitude}, {process.env.NODE_ENV} {process.env.REACT_APP_GOOGLE_MAP_API_KEY} */}
+            <div>{position.lat}, {position.lng}</div>
+            <div>{Coordinate.Latitude} , {Coordinate.Longitude}</div>
+            <div>{+Coordinate.Latitude} , {+Coordinate.Longitude}</div>
             <div style={{ height: '50vh', width: '50%' }}>
                 <GoogleMapReact
+                    onGoogleApiLoaded={handleLoad}
                     bootstrapURLKeys={{ key: GetAPIKey() }}
-                    center={GetCenter()}
-                    zoom={15}>
+                    defaultCenter={{lat: +Coordinate.Latitude, lng: +Coordinate.Longitude }}
+                    defaultZoom={15}>
                     <BreweryMapMarker lat={+Coordinate.Latitude} lng={+Coordinate.Longitude}  />
                 </GoogleMapReact>
             </div>
