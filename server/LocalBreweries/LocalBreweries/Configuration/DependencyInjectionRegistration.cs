e772adde4ced964281628aab8bf2cac465d03d7d
Kyle Lauffer <kcl5072@gmail.com>
2021-08-13 19:05:04 -0400

    adding data layer and UI adjustments

---
+++ server/LocalBreweries/LocalBreweries/Configuration/DependencyInjectionRegistration.cs
@@ -0,0 +1,14 @@
+﻿using LocalBreweries.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LocalBreweries.Api.Configuration
+{
+    public static class DependencyInjectionRegistration
+    {
+
+        public static void BootstrapDIMappings(this IServiceCollection services)
+        {
+            services.RegisterData();
+        }
+    }
+}
--- server/LocalBreweries/Service/Breweries/Queries/GetCommandler.cs
+++ server/LocalBreweries/Service/Breweries/Queries/GetCommandler.cs
@@ -19,22 +19,15 @@ namespace LocalBreweries.Service.Breweries.Queries
 
         public class GetBreweryHandler : IRequestHandler<GetBreweryRequest, MaybeResult<Brewery>>
         {
-            public Task<MaybeResult<Brewery>> Handle(GetBreweryRequest request, CancellationToken cancellationToken)
+            private readonly IBreweryRepository _breweryRepository;
+
+            public GetBreweryHandler(IBreweryRepository breweryRepository)
             {
-                MaybeResult<Brewery> result = new MaybeResult<Brewery>.Nothing();
-                if (request.Id % 2 == 0)//if number is even
-                {
-                    result = new MaybeResult<Brewery>.Just(new Brewery(
-                    id: request.Id,
-                    name: "Moe's",
-                    phoneNumber: "867-5309",
-                    type: "Fictional",
-                    url: "https://github.com/klauffer",
-                    address: new Address("700 Evergreen Terrace", "Springfield", "PA", "12345", "US"),
-                    coordinate: new Coordinate(40.291059, -76.8816961)));
-                }
-                return Task.FromResult(result);
+                _breweryRepository = breweryRepository;
             }
+
+            public async Task<MaybeResult<Brewery>> Handle(GetBreweryRequest request, CancellationToken cancellationToken) =>
+                await _breweryRepository.GetBrewery(request.Id);
 
         }
     }
